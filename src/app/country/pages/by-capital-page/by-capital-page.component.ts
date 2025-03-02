22eeb8eba63ac35b429211b845f3c9513db062b2
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-02 21:57:34 +0100

    refactor country list and search functionality; enhance population display and error handling

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from '../../../shared/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,20 +12,16 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (request.query === '') return [];
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
-
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCapital(query).subscribe((countries) => {
-      this.isLoading.set(false);
-      this.countries.set(countries);
-    });
-  }
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  });
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-country.interface';
 import { CounterMapper } from '../mappers/country.mapper';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -14,8 +14,11 @@ export class CountryService {
 
   searchByCapital(query: string) {
     const q = query.toLowerCase().trim();
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${q}`)
-      .pipe(map(CounterMapper.mapRestCountriesArrayToCountryArray));
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${q}`).pipe(
+      map(CounterMapper.mapRestCountriesArrayToCountryArray),
+      catchError((error) => {
+        return throwError(() => new Error(`No exist results for ${query}`));
+      })
+    );
   }
 }
