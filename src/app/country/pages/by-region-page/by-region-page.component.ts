e7fee9bc36532f31de33324023a0cf9b51f1a590
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-07 20:51:47 +0100

    refactor by-region-page component; implement linked signal for region query parameter handling and validation

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,10 +1,24 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { Region } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 import { NgClass } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+
+function validateQueryParam(queryParam: string) {
+  const validRegions: Record<string, Region> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+
+  return validRegions[queryParam.toLowerCase()] ?? 'Americas';
+}
 
 @Component({
   selector: 'app-by-region-page',
@@ -13,7 +27,9 @@ import { NgClass } from '@angular/common';
 })
 export class ByRegionPageComponent {
   countryService = inject(CountryService);
-  public regions: Region[] = [
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  regions: Region[] = [
     'Africa',
     'Americas',
     'Asia',
@@ -21,12 +37,19 @@ export class ByRegionPageComponent {
     'Oceania',
     'Antarctic',
   ];
-  selectedRegion = signal<Region | null>(null);
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
+  selectedRegion = linkedSignal<Region | null>(() =>
+    validateQueryParam(this.queryParam)
+  );
 
   countryResource = rxResource({
     request: () => ({ region: this.selectedRegion() }),
     loader: ({ request }) => {
       if (!request.region) return of([]);
+
+      this.router.navigate(['/country/by-region'], {
+        queryParams: { region: request.region },
+      });
 
       return this.countryService.searchByRegion(request.region);
     },
